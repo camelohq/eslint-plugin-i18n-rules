516d930a5ebdad4e6ba27c4e036ff0fcf5c9c955
tdhuan <dinhhuantran3010@gmail.com>
2025-09-20 23:50:17 +0700

    feat: migrate from tsup to tsdown bundler
    - Replace tsup with tsdown for improved bundling performance
    - Update package.json build script to use tsdown
    - Rename tsup.config.ts to tsdown.config.ts
    - Enhanced bundling with rolldown runtime
    - Improved module handling and tree-shaking

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,32 +1,32 @@
-import * as _typescript_eslint_utils_dist_ts_eslint_Rule from '@typescript-eslint/utils/dist/ts-eslint/Rule';
+import * as _typescript_eslint_utils_dist_ts_eslint_Rule0 from "@typescript-eslint/utils/dist/ts-eslint/Rule";
 
+//#region src/index.d.ts
 declare const _default: {
-    rules: {
-        "no-hardcoded-jsx-text": _typescript_eslint_utils_dist_ts_eslint_Rule.RuleModule<"noHardcoded", [{
-            ignoreLiterals?: string[] | undefined;
-            caseSensitive?: boolean | undefined;
-            trim?: boolean | undefined;
-        }], _typescript_eslint_utils_dist_ts_eslint_Rule.RuleListener>;
-        "no-hardcoded-jsx-attributes": _typescript_eslint_utils_dist_ts_eslint_Rule.RuleModule<"noHardcodedAttr", [{
-            ignoreLiterals?: string[] | undefined;
-            caseSensitive?: boolean | undefined;
-            trim?: boolean | undefined;
-            ignoreComponentsWithTitle?: string[] | undefined;
-        }], _typescript_eslint_utils_dist_ts_eslint_Rule.RuleListener>;
+  rules: {
+    "no-hardcoded-jsx-text": _typescript_eslint_utils_dist_ts_eslint_Rule0.RuleModule<"noHardcoded", [{
+      ignoreLiterals?: string[] | undefined;
+      caseSensitive?: boolean | undefined;
+      trim?: boolean | undefined;
+    }], _typescript_eslint_utils_dist_ts_eslint_Rule0.RuleListener>;
+    "no-hardcoded-jsx-attributes": _typescript_eslint_utils_dist_ts_eslint_Rule0.RuleModule<"noHardcodedAttr", [{
+      ignoreLiterals?: string[] | undefined;
+      caseSensitive?: boolean | undefined;
+      trim?: boolean | undefined;
+      ignoreComponentsWithTitle?: string[] | undefined;
+    }], _typescript_eslint_utils_dist_ts_eslint_Rule0.RuleListener>;
+  };
+  configs: {
+    recommended: {
+      plugins: string[];
+      rules: {
+        "i18n-rules/no-hardcoded-jsx-text": string;
+        "i18n-rules/no-hardcoded-jsx-attributes": (string | {
+          ignoreLiterals: string[];
+          caseSensitive: boolean;
+          trim: boolean;
+        })[];
+      };
     };
-    configs: {
-        recommended: {
-            plugins: string[];
-            rules: {
-                "i18n-rules/no-hardcoded-jsx-text": string;
-                "i18n-rules/no-hardcoded-jsx-attributes": (string | {
-                    ignoreLiterals: string[];
-                    caseSensitive: boolean;
-                    trim: boolean;
-                })[];
-            };
-        };
-    };
+  };
 };
-
-export { _default as default };
+export = _default;
